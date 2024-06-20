9f60fc400d375b6bd12f80ff47af34ccfce9cb8d
myuserlee <myuserlee@gmail.com>
2024-06-20 22:48:50 +0900

    이미지회전되는문제해결

--- CoreImageApp/CoreImageApp/PhotoPickView.swift
+++ CoreImageApp/CoreImageApp/PhotoPickView.swift
@@ -24,108 +24,139 @@ struct PhotoPickView: View {
                     .fontWeight(.bold)
                     .foregroundColor(.black)
                 
-                //물음표 사각형
-                ZStack {
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 300, height: 380)
-                        .background(.white)
-                        .shadow(color: .black.opacity(0.15), radius: 7.5, x: 0, y: 2)
-                    
-                    if let selectedImage = selectedImage {
-                        Image(uiImage: selectedImage)
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 250, height: 350)
-                            .clipped()
-                    } else {
-                        Image(systemName: "fork.knife")
-                            .font(.system(size: 100))
-                            .foregroundColor(.gray)
-                    }
-                }
-                .padding(20)
+                // 선택된 이미지 보여주기
+                showSelectedImage()
                 
-                // 사진 불러오기 : 카메라로 촬영하기
                 HStack{
-                    Button{
-                        isImagePickerPresented = true
-                    } label: {
-                        HStack(alignment: .center, spacing: 4) {
-                            VStack{
-                                Image(systemName: "camera.fill")
-                                    .font(.system(size: 50))
-                                Text("직접 촬영하기")
-                                    .font(.body)
-                                    .fontWeight(.bold)
-                                    .foregroundColor(Color.point)
-                            }
-                        }
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 14)
-                    }
-                    .sheet(isPresented: $isImagePickerPresented) {
-                        ImagePicker(isPresented: $isImagePickerPresented, selectedImage: $selectedImage)
-                    }
+                    // 사진 불러오기 : 카메라로 촬영하기
+                    takePhotoButton()
                     
-                    PhotosPicker(selection: Binding(get: {
-                        selectedItem
-                    }, set: { newItem in
-                        selectedItem = newItem
-                        if let newItem = newItem {
-                            Task {
-                                do {
-                                    let data = try await newItem.loadTransferable(type: Data.self)
-                                    guard let data = data else {
-                                        print("No data found")
-                                        return
-                                    }
-                                    selectedImage = UIImage(data: data)
-                                } catch {
-                                    print("Error loading image: \(error.localizedDescription)")
-                                }
-                            }
-                        }
-                    }), matching: .images, photoLibrary: .shared()) {
-                        HStack(alignment: .center, spacing: 4) {
-                            VStack{
-                                Image(systemName: "photo.fill")
-                                    .font(.system(size: 50))
-                                Text("사진 선택하기")
-                                    .font(.body)
-                                    .fontWeight(.bold)
-                                    .foregroundColor(Color.point)
-                            }
-                        }
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 14)
-                    }
-                    
+                    // 사진 불러오기 : 갤러리에서 이미지 선택
+                    selectPhotoButton()
                 }
             }
             .padding(10)
         }
+        
         if selectedImage != nil {
-            NavigationLink(destination: FilterListView(selectedImage: selectedImage!)) {
-                Text("필터 적용하기")
-                    .font(.body)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .frame(width: 359, height: 50, alignment: .center)
-                    .background(Color(UIColor(named: "PointColor")!))
-                    .cornerRadius(12)
+            NavigationLink(destination: FilterListView(selectedImage: fixOrientation(img: selectedImage!))) {
+                designedButton("필터 적용하기", isAble: true)
             }
         } else {
-            Text("사진을 선택해주세요")
+            designedButton("사진을 선택해주세요", isAble: false)
+        }
+    }
+    
+    // 선택된 이미지 보여주기
+    @ViewBuilder
+    func showSelectedImage() -> some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(width: 300, height: 380)
+                .background(.white)
+                .shadow(color: .black.opacity(0.15), radius: 7.5, x: 0, y: 2)
+            
+            if let selectedImage = selectedImage {
+                Image(uiImage: selectedImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 250, height: 350)
+                    .clipped()
+            } else {
+                Image(systemName: "fork.knife")
+                    .font(.system(size: 100))
+                    .foregroundColor(.gray)
+            }
+        }
+        .padding(20)
+    }
+    
+    // 사진 불러오기 : 카메라로 촬영하기
+    @ViewBuilder
+    func takePhotoButton() -> some View {
+        Button{
+            isImagePickerPresented = true
+        } label: {
+            HStack(alignment: .center, spacing: 4) {
+                VStack{
+                    Image(systemName: "camera.fill")
+                        .font(.system(size: 50))
+                        .padding(2)
+                    Text("직접 촬영하기")
+                        .font(.body)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.point)
+                }
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 14)
+        }
+        .sheet(isPresented: $isImagePickerPresented) {
+            ImagePicker(isPresented: $isImagePickerPresented, selectedImage: $selectedImage)
+        }
+    }
+    
+    // 사진 불러오기 : 갤러리에서 이미지 선택
+    @ViewBuilder
+    func selectPhotoButton() -> some View {
+        PhotosPicker(selection: Binding(get: {
+            selectedItem
+        }, set: { newItem in
+            selectedItem = newItem
+            if let newItem = newItem {
+                Task {
+                    do {
+                        let data = try await newItem.loadTransferable(type: Data.self)
+                        guard let data = data else {
+                            print("No data found")
+                            return
+                        }
+                        selectedImage = UIImage(data: data)
+                    } catch {
+                        print("Error loading image: \(error.localizedDescription)")
+                    }
+                }
+            }
+        }), matching: .images, photoLibrary: .shared()) {
+            HStack(alignment: .center, spacing: 4) {
+                VStack{
+                    Image(systemName: "photo.fill")
+                        .font(.system(size: 50))
+                        .padding(2)
+                    Text("사진 선택하기")
+                        .font(.body)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.point)
+                }
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 14)
+        }
+    }
+    
+    // 버튼 디자인
+    @ViewBuilder
+    func designedButton(_ text: String, isAble: Bool) -> some View {
+        if isAble {
+            Text(text)
+                .font(.body)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .frame(width: 359, height: 50, alignment: .center)
+                .background(Color(UIColor(named: "PointColor")!))
+                .cornerRadius(12)
+        } else {
+            Text(text)
                 .font(.body)
                 .fontWeight(.bold)
                 .foregroundColor(.white)
                 .frame(width: 359, height: 50, alignment: .center)
                 .background(Color.gray)
                 .cornerRadius(12)
-                .disabled(true)
         }
     }
+    
 }
 
 #Preview {
